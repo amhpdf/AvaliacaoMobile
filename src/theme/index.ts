0b95c40186dd8c772f5f3b026b1f549dc8ebf896
Wallisson Martins <wallissonmartins37@gmail.com>
2024-03-07 17:52:02 -0300

    remove font-sizes theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,15 +3,6 @@ const theme = {
     PRIMARY_COLOR: "#028288",
     SECOND_COLOR: "#404758",
   },
-
-  FONT_SIZE: {
-    XS: "12px",
-    SM: "14px",
-    MD: "16px",
-    LG: "18px",
-    XL: "20px",
-    XXL: "22px",
-  },
 };
 
 export default theme;
