f74636160db352f4b91b3fd602defb5c994f1bf6
JosueTavaress <josueferreira8.jf@gmail.com>
2024-03-08 14:13:01 -0300

    CloseButton (feat): create button close fixed

---
+++ app/components/CloseButton/styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  top: 20px;
+  right: 15px;
+  background-color: #e74c3c;
+  padding: 10px;
+  border-radius: 20px;
+  position: absolute;
+  align-items: center;
+  justify-content: center;
+  elevation: 2;
+  shadow-color: #000;
+  shadow-offset: 0px 2px;
+  shadow-opacity: 0.23;
+  shadow-radius: 2.62px;
+`;
