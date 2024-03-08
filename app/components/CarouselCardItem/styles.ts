6963037914b5685eb21e925c5a0586e7d5fb29bc
JosueTavaress <josueferreira8.jf@gmail.com>
2024-03-08 14:10:36 -0300

    Carousel (refactor): add new height img and margin

--- app/components/CarouselCardItem/styles.ts
+++ app/components/CarouselCardItem/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 
 export const Img = styled.Image`
-  height: 250px;
-  border-radius: 20%;
+  height: 220px;
+  border-radius: 12%;
+  margin-bottom: 160px;
 `;
