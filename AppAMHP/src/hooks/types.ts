5c72b6ef478328a38ffe91759842f717fc2fe107
Meiriellen <meiriellen@decisaosistemas.com.br>
2024-03-08 14:21:00 -0300

    foram criado as páginas de home, convenios e noticias

---
+++ AppAMHP/src/hooks/types.ts
@@ -0,0 +1,15 @@
+export interface IBanner {
+    id: number;
+  }
+
+  export interface INews {
+    id: number;
+    titulo: string;
+    imagem: string;
+    dataPublicacao: string;
+  }
+
+  export interface IConvenio {
+    id: number;
+    nomeConvenio: string;
+  }
---
+++ AppAMHP/src/styles/theme.ts
@@ -0,0 +1,6 @@
+// colors.ts
+export const colors = {
+    navy: '#001F3F',
+    white: '#FFFFFF',
+    lightGray: '#D3D3D3',
+  };
